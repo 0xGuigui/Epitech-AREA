9ca6c520a9c56446542c3fe232f07dc538deb0dc
Florian Garnier <florian.garnier@epitech.eu>
2022-09-21 11:00:59 +0200

    Add basic about.json route

--- Server/index.js
+++ Server/index.js
@@ -3,3 +3,15 @@ const app = require('express')()
 app.listen(8080, () => {
     console.log("Server ready on port 8080")
 })
+
+app.get('/about.json', (req, res) => {
+    res.json({
+        client: {
+            host: req.socket.remoteAddress
+        },
+        server: {
+            current_time: Date.now(),
+            services: []
+        }
+    })
+})
