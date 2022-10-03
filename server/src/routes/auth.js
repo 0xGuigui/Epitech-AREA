042ad53e3e2a9966716a3d753bac47a405a6ab1c
Gagonlaire <paul.gervais@epitech.eu>
2022-10-03 15:28:15 +0200

    Add fully working register/login process, working on email verification

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -10,26 +10,47 @@ const {
 } = require('../models/validationModels')
 
 module.exports = (area) => {
+    area.app.post('/register', ...registerValidator, async (req, res) => {
+        if (!payloadValidator(req, res)) return
+
+        const {email, password, username} = req.body
+
+        let hashedPassword = await hashPassword(password)
+        let newUser = new area.mongoModels["User"]({
+            username: username,
+            email: email,
+            password: hashedPassword,
+        })
+
+        newUser.save().then(() => {
+            return res.status(200).json({message: 'User created'})
+        }).catch((err) => {
+            return res.status(500).json({message: err.message})
+        })
+    })
+
     area.app.post('/login', ...loginValidator, async (req, res) => {
         if (!payloadValidator(req, res)) return
 
-        const {email, password} = req.body
-        let user = await mongoose.model('User').findOne({email: email}).exec()
+        let user = await mongoose.model('User').findOne({email: req.body.email}).exec()
         if (!user) {
             return res.status(401).json({message: 'Invalid credentials'})
         }
-        let passwordMatch = await comparePassword(password, user.password)
+        if (!user.verified) {
+            return res.status(401).json({message: 'User not verified'})
+        }
+        let passwordMatch = await comparePassword(req.body.password, user.password)
         if (!passwordMatch) {
             return res.status(401).json({message: 'Invalid credentials'})
         }
-        // Check sent information in db, here we assume that everything is ok
-        let accessToken = jwt.sign({
-            userId: 1,
+        let payload = {
+            userId: user.id,
             username: req.body.username,
-            email: req.body.email
-        }, area.config.jwtAccessSecret, {expiresIn: '5m'})
+            isAdmin: user.isAdministrator
+        }
+        let accessToken = jwt.sign(payload, area.config.jwtAccessSecret, {expiresIn: '5m'}, null)
 
-        jwt.sign({}, area.config.jwtRefreshSecret, {expiresIn: '1d'}, (err, refreshToken) => {
+        jwt.sign(payload, area.config.jwtRefreshSecret, {expiresIn: '1d'}, (err, refreshToken) => {
             if (err) {
                 res.status(500).send({error: 'Internal server error'})
                 return
@@ -49,19 +70,19 @@ module.exports = (area) => {
             const refreshToken = req.cookies.jwt;
 
             // Verifying refresh token
-            jwt.verify(refreshToken, area.config.jwtRefreshSecret, {}, (err, decoded) => {
+            jwt.verify(refreshToken, area.config.jwtRefreshSecret, {}, async (err, decoded) => {
                 if (err) {
-                    // Wrong Refresh Token
                     return res.status(406).json({message: 'Unauthorized'});
                 } else {
-                    // Correct token we send a new access token
+                    let user = await mongoose.model('User').findById(decoded.userId).exec()
+
                     const accessToken = jwt.sign({
-                        userId: decoded.userId,
-                        username: decoded.username,
-                        email: decoded.email
+                        userId: user.id,
+                        username: user.username,
+                        isAdmin: user.isAdminiastrator,
                     }, area.config.jwtAccessSecret, {
                         expiresIn: '5m'
-                    });
+                    }, null);
                     return res.json({accessToken});
                 }
             })
@@ -70,37 +91,30 @@ module.exports = (area) => {
         }
     })
 
-    area.app.post('/register', ...registerValidator, async (req, res) => {
-        if (!payloadValidator(req, res)) return
-
-        const {email, password, username} = req.body
-
-        let newUser = new mongoose.model('User')({
-            username: username,
-            email: email,
-            password: await hashPassword(password),
-        })
-
-        newUser.save().then(() => {
-            res.status(200).json({message: 'User created'})
-        }).catch((err) => {
-            res.status(500).json({message: 'Internal server error'})
-        })
-        res.status(200).json({message: 'User created'})
-    })
-
     area.app.post('/reset-password', ...resetPasswordValidator, (req, res) => {
         if (!payloadValidator(req, res)) return
 
-        // Check sent information in db, here we assume that everything is ok
-        area.blacklistJWT(1)
         res.send('Reset password page');
     })
 
-    area.app.post('/update-password', ...updatePasswordValidator, (req, res) => {
+    area.app.post('/update-password', ...updatePasswordValidator, async (req, res) => {
         if (!payloadValidator(req, res)) return
 
-        area.blacklistJWT(1)
-        res.send('Update password page');
+        let user = await mongoose.model('User').findById(req.jwt.userId).exec()
+
+        if (!user) {
+            return res.status(500).json({message: 'User no longer exists'})
+        }
+        let passwordMatch = await comparePassword(req.body.password, user.password)
+        if (!passwordMatch) {
+            return res.status(401).json({message: 'Invalid credentials'})
+        }
+        user.password = await hashPassword(req.body.newPassword)
+        user.save().then(() => {
+            area.blacklistJWT(user.id)
+            return res.status(200).json({message: 'Password updated'})
+        }).catch((err) => {
+            return res.status(500).json({message: err.message})
+        })
     })
 }
