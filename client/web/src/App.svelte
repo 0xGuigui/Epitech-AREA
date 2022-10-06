7fcb2eca1886cc89479093b8d001fdae7485de69
julien <julien.hennion@epitech.eu>
2022-10-06 14:29:53 +0200

    Added initial login page

--- client/web/src/App.svelte
+++ client/web/src/App.svelte
@@ -1,45 +1,27 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
+
+  const form = useForm();
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<form use:form style="display: flex; flex-direction: column">
+  <h1>Login</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <HintGroup for="email">
+    <Hint on="required">This is a mandatory field</Hint>
+    <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+  </HintGroup>
+  <input type="email" name="email" use:validators={[required, email]} />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <input type="password" name="password" use:validators={[required]} />
+  <Hint for="password" on="required">This is a mandatory field</Hint>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+  <button disabled={!$form.valid}>Login</button>
+</form>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  :global(.touched:invalid) {
+    border-color: red;
+    outline-color: red;
   }
 </style>
