f19c31ae111de14551f8d0ef9abf558b716b3b09
julien <julien.hennion@epitech.eu>
2022-10-06 15:14:31 +0200

    Added bind feature on the fields and fixed graphics problems

--- client/web/src/App.svelte
+++ client/web/src/App.svelte
@@ -2,19 +2,26 @@
   import { useForm, validators, HintGroup, Hint, email, required } from "svelte-use-form";
 
   const form = useForm();
+  let mail = '';
+  let password = '';
+
+  (async () => {
+    const response = await fetch(`http://localhost:8080/about.json`);
+    const data = await response.json();
+    console.log(data);
+  })();
 </script>
 
 <form use:form style="display: flex; flex-direction: column">
   <h1>Login</h1>
 
   <HintGroup for="email">
-    <Hint on="required">This is a mandatory field</Hint>
+    <Hint on="required">Mandatory field</Hint>
     <Hint on="email" hideWhenRequired>Email is not valid</Hint>
   </HintGroup>
-  <input type="email" name="email" use:validators={[required, email]} />
-
-  <input type="password" name="password" use:validators={[required]} />
-  <Hint for="password" on="required">This is a mandatory field</Hint>
+  <input bind:value={mail} type="email" name="email" placeholder="enter your email" style="margin-bottom: 8px" use:validators={[required, email]} />
+  <Hint for="password" on="required">Mandatory field</Hint>
+  <input bind:value={password} type="password" name="password" placeholder="enter your password" style="margin-bottom: 8px" use:validators={[required]} />
 
   <button disabled={!$form.valid}>Login</button>
 </form>
