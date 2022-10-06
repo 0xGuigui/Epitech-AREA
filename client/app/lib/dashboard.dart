c27b4b85cd93d013a0e57c5676ed89e1c8777107
mflr0 <mflr0@onionmail.org>
2022-10-06 11:14:14 +0200

    Updated dashboard in Flutter, added drawer, but it doesn't work

--- client/app/lib/dashboard.dart
+++ client/app/lib/dashboard.dart
@@ -10,13 +10,55 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      resizeToAvoidBottomInset: false,
-      body: const Center(
-        child: Text('Dashboard'),
+    return MaterialApp(
+      title: "Dashboard",
+      home: Theme(
+        data: ThemeData(
+          primarySwatch: Colors.blue,
+          brightness: Brightness.dark,
+        ),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Dashboard'),
+            // add button to open a drawer from the left and add inside the drawer a button to close the drawer
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Drawer(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: <Widget>[
+                      const DrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: Text('Drawer Header'),
+                      ),
+                      ListTile(
+                        title: const Text('Item 1'),
+                        onTap: () {
+                          // Update the state of the app.
+                          // ...
+                        },
+                      ),
+                      ListTile(
+                        title: const Text('Item 2'),
+                        onTap: () {
+                          // Update the state of the app.
+                          // ...
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          resizeToAvoidBottomInset: false,
+          body: const Center(
+            child: Text('Dashboard'),
+          ),
+        ),
       ),
     );
   }
