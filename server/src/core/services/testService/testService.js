3089fff8a3f364c0e1c0651edcb57a16af978dc2
Gagonlaire <paul.gervais@epitech.eu>
2022-10-10 16:06:11 +0200

    Some code improvements, rework service manager

--- server/src/core/services/testService/testService.js
+++ server/src/core/services/testService/testService.js
@@ -1,2 +1,12 @@
-module.exports = (area) => {
+const {Service, Action, Reaction} = require('../serviceComponents')
+
+module.exports = (area, servicesManager) => {
+    let testService = new Service('testService')
+    let testAction = new Action('testAction', false)
+    let testReaction = new Reaction('testReaction')
+
+    testService.addReaction(testReaction)
+    testService.addAction(testAction)
+
+    servicesManager.addService(testService)
 }
--- server/src/utils/dynamicLoader.js
+++ server/src/utils/dynamicLoader.js
@@ -22,21 +22,4 @@ let dynamicLoader = (area, path) => {
     })
 }
 
-let servicesLoader = (area, path) => {
-    let absolutePath = path[0] === '/' ? path : pathBuilder.join(__dirname, path);
-
-    fs.readdirSync(absolutePath).forEach(file => {
-        let filePath = pathBuilder.join(absolutePath, file);
-        let stat = fs.statSync(filePath);
-
-        if (stat && stat.isDirectory()) {
-            let fileContent = require(pathBuilder.join(filePath, file + '.js'));
-
-            if (typeof fileContent === 'function') {
-                fileContent(area);
-            }
-        }
-    })
-}
-
-module.exports = {dynamicLoader, servicesLoader}
+module.exports = {dynamicLoader}
