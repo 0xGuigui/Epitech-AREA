0967cc55efabe051ef479965af6bb4f52936d56e
julien <julien.hennion@epitech.eu>
2022-10-10 15:05:40 +0200

    Fix missing files

---
+++ client/web/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Login from "./lib/login_page.svelte";
+  import SignUp from "./lib/sign-up_page.svelte";
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+  <div>
+    <Route path="/">
+      <h1>Welcome to AREA</h1>
+      <Link to="login">Login</Link>
+      <Link to="signup">/ Sign-up</Link>
+    </Route>
+    <Route path="/login" component="{Login}" />
+    <Route path="/signup" component="{SignUp}" />
+  </div>
+</Router>
