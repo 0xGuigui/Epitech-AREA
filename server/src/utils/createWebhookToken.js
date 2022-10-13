f0da5f2e9cec7ac1a25bd966dcab41fc3376fbd8
Gagonlaire <paul.gervais@epitech.eu>
2022-10-13 10:23:14 +0200

    Add the possibility to have webhook actions

---
+++ server/src/utils/createWebhookToken.js
@@ -0,0 +1,9 @@
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+module.exports = (webhookData) => {
+    if (typeof webhookData !== 'object') {
+        throw new Error('Invalid webhook data')
+    }
+    return jwt.sign(webhookData, config.jwtSecret, {}, null)
+}
