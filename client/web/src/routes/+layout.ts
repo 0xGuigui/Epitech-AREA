97ddc3514ae2caf0f758de506ffeb445791f3f68
Gagonlaire <paul.gervais@epitech.eu>
2022-10-24 00:27:22 +0200

    Add advanced svelte layout example

---
+++ client/web/src/routes/+layout.ts
@@ -0,0 +1,16 @@
+import * as store from '../store'
+
+// Must be true to avoid undefined localStorage
+export const ssr = false;
+
+export function load() {
+    let accessToken: string | null = localStorage.getItem('accessToken')
+
+    if (accessToken) {
+        store.loggedIn.set(true)
+        store.accessToken.set(accessToken)
+    }
+    return {
+        accessToken: accessToken,
+    }
+}
