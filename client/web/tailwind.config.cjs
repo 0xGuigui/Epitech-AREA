83fcee189c22cd8dd90f4281967f3afb23e6af19
Gagonlaire <paul.gervais@epitech.eu>
2022-10-25 18:02:22 +0200

    Add reset-password process

--- client/web/tailwind.config.cjs
+++ client/web/tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	mode: 'jit',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'area': {
+					'blue': '#3a519e',
+					'purple': '#695690',
+					'red': '#e02527'
+				}
+			}
+		}
+	},
+	plugins: []
+};
