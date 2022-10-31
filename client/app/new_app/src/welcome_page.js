72f396e657df1a0067df5349224784013348ffe4
mflr0 <mflr0@onionmail.org>
2022-10-31 15:03:47 +0100

    Updated React Native project

--- client/app/new_app/src/welcome_page.js
+++ client/app/new_app/src/welcome_page.js
@@ -1,9 +1,11 @@
 import {View, Text, StyleSheet} from 'react-native';
+import {useEffect, useState} from 'react';
 
 export default function WelcomePage() {
   return (
     <View style={styles.welcomePage}>
-      <Text style={styles.text}> Welcome to the app </Text>
+      <Text style={{...styles.text}}>Welcome to the AREA Project!</Text>
+      <Text style={{...styles.text2}}>TumbleWeed Spotify</Text>
     </View>
   );
 }
@@ -11,11 +13,24 @@ export default function WelcomePage() {
 const styles = StyleSheet.create({
   welcomePage: {
     position: 'absolute',
-    top: '50%',
-    left: '50%',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
   text: {
+    fontSize: 40,
+    fontWeight: '700',
     textAlign: 'center',
     color: 'white',
+    marginTop: 250,
+  },
+  text2: {
+    fontSize: 40,
+    fontWeight: '700',
+    textAlign: 'center',
+    color: 'white',
+    marginTop: 250,
+    fontFamily: 'GothamMedium',
   },
 });
