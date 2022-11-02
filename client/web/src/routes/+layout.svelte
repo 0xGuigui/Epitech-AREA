408eb35d539565123ae121a52201a0c144ddf508
Gagonlaire <paul.gervais@epitech.eu>
2022-11-03 00:02:44 +0100

    Minor improvements on front

--- client/web/src/routes/+layout.svelte
+++ client/web/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import "../app.css";
+	let AOS;
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		AOS = (await import('aos')).default;
+		AOS.init({
+			once: 'true'
+		});
+	});
 </script>
 
 <slot />
