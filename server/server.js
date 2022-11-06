bfe24064ba461ad56f17ceb202d047597ac8eb83
Gagonlaire <paul.gervais@epitech.eu>
2022-11-06 19:35:44 +0100

    Add config loader (not fully implemented yet)

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
 const area = require('./src/area')()
+const logger = require('node-color-log')
 
 // Start the server
-area.connectToDB().then(() => {
-    area.start(() => {
-        console.log(`Server started on port ${area.config.port}: http://localhost:${area.config.port}`)
-    })
-}).catch(err => {
-    console.error(err)
+area.start(() => {
+    logger.info(`Server started on port ${area.config.port}`)
 })
--- server/src/area.js
+++ server/src/area.js
@@ -1,7 +1,8 @@
 const path = require('path')
 const mongoose = require('mongoose')
 const express = require('express')
-const config = require('../config')
+const configLoader = require('./config-loader')
+const logger = require('node-color-log')
 const {dynamicLoader, loadFile} = require('./utils/dynamicLoader')
 const {hashPassword} = require('./utils/passwordHashing')
 const JwtDenyList = require('./core/jwtDenyList')
@@ -11,14 +12,16 @@ const ServicesManager = require('./core/services/servicesManager')
 class AREA {
     constructor() {
         this.app = express()
-        this.config = config
         this.unprotectedRoutes = ["login", "refresh", "register", "reset-password", "about.json", "verify", "webhook"]
 
-        // Check for required fields in the config
-        this.checkConfig()
+        // Call dotenv to init env file
+        configLoader.initEnv()
 
         // Instantiate the services manager
         this.servicesManager = new ServicesManager(this)
+
+        // Load all configs
+        this.checkConfig()
 
         // Load all middlewares and routes
         dynamicLoader(this, path.join(__dirname, 'api/middlewares'), ['dynamic.js', 'others.js', 'error.js'])
@@ -34,15 +37,36 @@ class AREA {
         this.jwtDenyList = new JwtDenyList()
 
         // Instantiate email sender
-        this.mailSender = new MailSender(config)
+        // @TODO: update with new config
+        this.mailSender = new MailSender({})
     }
 
     checkConfig() {
-        Object.entries(this.config).forEach(([key, value]) => {
-            if (value === undefined) {
-                throw new Error(`Missing config field: ${key}`)
+        let serviceCount = 0
+        let erroredServicesCount = 0
+
+        configLoader.checkEnvConfig(require('../config.json').baseConfig, true);
+        logger.success("Server environment variables successfully loaded")
+        this.servicesManager.getServices().forEach(service => {
+            serviceCount++
+            if (service.envConfig) {
+                let errors = configLoader.checkEnvConfig(service.envConfig, false);
+
+                if (errors.length > 0) {
+                    service.disable()
+                    erroredServicesCount++
+                    logger.warn(`${service.name} service has been disabled because of missing variables:`)
+                    errors.forEach(error => console.log(error))
+                    process.exit(1)
+                }
             }
         })
+
+        if (erroredServicesCount > 0) {
+            logger.warn(`${erroredServicesCount} service(s) have been disabled because of missing variables.`)
+        } else {
+            logger.info(`${serviceCount} services out of ${serviceCount} have been loaded.`)
+        }
     }
 
     async connectToDB() {
@@ -59,6 +83,7 @@ class AREA {
             admin: true,
         }
 
+        await this.connectToDB()
         await mongoose.models.User.findOneAndUpdate({username: "admin"}, adminUserData, {
             upsert: true, setDefaultsOnInsert: true
         }).exec()
