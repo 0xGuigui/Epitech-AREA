45a527bd3a4aab95056daf1b9926704c90d3482a
Florian Garnier <florian.garnier@epitech.eu>
2022-11-06 16:51:08 +0100

    Update projet architecture

--- client/app/src/main/mainRoute.js
+++ client/app/src/main/mainRoute.js
@@ -2,12 +2,14 @@ import AppFooter from "./footer/footer";
 import {Route, Routes, useLocation, useNavigate} from "react-router-native";
 import MainPage from './mainPage/mainPage'
 import Account from "./account";
+import CreateArea from "./createArea/create";
 
 export default function MainRoute({ userInfo, setUserInfo }) {
 	return (
 		<>
 			<Routes>
 				<Route path="/main" element={<MainPage userInfo={userInfo} />} />
+				<Route path="/create" element={<CreateArea />} />
 				<Route path="/account" element={<Account userInfo={userInfo} setUserInfo={setUserInfo}/>} />
 			</Routes>
 			<AppFooter userInfo={userInfo} />
