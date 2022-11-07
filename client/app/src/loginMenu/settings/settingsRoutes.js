f6187df40876c858bc45901c6d1fa5f36b3b43a7
mflr0 <mflr0@onionmail.org>
2022-11-07 17:41:04 +0100

    Updated settings routes, but it doesn't work

--- client/app/src/loginMenu/settings/settingsRoutes.js
+++ client/app/src/loginMenu/settings/settingsRoutes.js
@@ -1,11 +1,24 @@
-import {Route, Routes} from "react-router-native";
+import {Route, Routes, useLocation} from "react-router-native";
 import Settings from "./settings";
 import ServerSelector from "./serverSelector";
+import {useContext, useEffect, useState} from "react";
+import {HistoryContext} from "../../historyContext";
 
 export default function SettingsRoutes({userInfo}) {
+	const history = useContext(HistoryContext)
+	const location = useLocation()
+	const [prevHistory, setPrevHistory] = useState(history.prev)
+
+	useEffect(() => {
+		if (location.pathname === '/settings' && history.prev.split('/')[1] !== 'settings')
+			setPrevHistory(history.path.split('/')[1] !== 'settings' ? history.prev : history.path)
+	}, [location])
+
+	console.log(prevHistory)
+
 	return (
 		<Routes>
-			<Route exact path='/' element={<Settings />} />
+			<Route exact path='/' element={<Settings prevHistory={prevHistory} />} />
 			<Route path='/server' element={<ServerSelector />} />
 		</Routes>
 	)
