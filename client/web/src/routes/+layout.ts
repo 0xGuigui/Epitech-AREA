d67c0632faa6b0c230845952a6582360d460f37b
Gagonlaire <paul.gervais@epitech.eu>
2022-11-08 22:55:40 +0100

    Improve front

--- client/web/src/routes/+layout.ts
+++ client/web/src/routes/+layout.ts
@@ -1,16 +1,3 @@
-import * as store from '../store'
-
-// Must be true to avoid undefined localStorage
-export const ssr = false;
-
 export function load() {
-    const accessToken: string | null = localStorage.getItem('accessToken')
-
-    if (accessToken) {
-        store.loggedIn.set(true)
-        store.accessToken.set(accessToken)
-    }
-    return {
-        accessToken: accessToken,
-    }
+    return {}
 }
