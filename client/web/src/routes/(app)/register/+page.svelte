a3c9d8be5c0fc30c483fe94c03731f557505f2c3
julien <julien.hennion@epitech.eu>
2022-11-08 14:00:22 +0100

    Changed focus on input forms and added /me/actions

--- client/web/src/routes/(app)/register/+page.svelte
+++ client/web/src/routes/(app)/register/+page.svelte
@@ -56,10 +56,22 @@
     <div data-aos="fade-up"
          class="shadow-2xl w-[400px] h-[600px] backdrop-blur-sm bg-white/30 rounded-3xl">
         <form class="flex flex-col justify-center items-center h-full" on:submit={handleSubmit}>
+            <div class="my-5">
+                <label class="block font-bold text-white" for="username">Username</label>
+                <input class="rounded focus:ring-2 focus:outline-none focus:ring-area-header px-1"
+                       id="username"
+                       name="username"
+                       on:change={handleChange}
+                       bind:value={$form.username}
+                />
+                {#if $errors.username}
+                    <small data-aos="fade-right" data-aos-duration="500" class="block flex justify-center text-red-600 font-semibold">{$errors.username}</small>
+                {/if}
+            </div>
             <h1 class="flex my-5 justify-center text-4xl font-bold text-white">Register</h1>
-                <div class="">
+            <div class="">
                     <label class="block font-bold text-white" for="email">Email</label>
-                    <input class="rounded"
+                    <input class="rounded focus:ring-2 focus:outline-none focus:ring-area-header px-1"
                            id="email"
                            name="email"
                            on:change={handleChange}
@@ -69,21 +81,9 @@
                         <small data-aos="fade-right" data-aos-duration="500" class="block flex justify-center text-red-600 font-semibold">{$errors.email}</small>
                     {/if}
                 </div>
-                <div class="my-5">
-                    <label class="block font-bold text-white" for="username">Username</label>
-                    <input class="rounded"
-                           id="username"
-                           name="username"
-                           on:change={handleChange}
-                           bind:value={$form.username}
-                    />
-                    {#if $errors.username}
-                        <small data-aos="fade-right" data-aos-duration="500" class="block flex justify-center text-red-600 font-semibold">{$errors.username}</small>
-                    {/if}
-                </div>
                 <div class="">
                     <label class="block font-bold text-white" for="password">Password</label>
-                    <input class="rounded"
+                    <input class="rounded focus:ring-2 focus:outline-none focus:ring-area-header px-1"
                            id="password"
                            name="password"
                            on:change={handleChange}
@@ -92,11 +92,12 @@
                     />
                     {#if $errors.password}
                         <small data-aos="fade-right" data-aos-duration="500" class="block flex justify-center text-red-600 font-semibold">{$errors.password}</small>
+
                     {/if}
                 </div>
                 <div class="my-5">
                     <label class="block font-bold text-white" for="confirmPassword">Confirm Password</label>
-                    <input class="rounded"
+                    <input class="rounded focus:ring-2 focus:outline-none focus:ring-area-header px-1"
                            id="confirmPassword"
                            name="confirmPassword"
                            on:change={handleChange}
