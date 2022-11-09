7b149c5292e056f5d3695db2ff45e63c5af0666f
Florian Garnier <florian.garnier@epitech.eu>
2022-11-09 22:42:06 +0100

    Fix userId on check-token

--- server/src/api/routes/others.js
+++ server/src/api/routes/others.js
@@ -95,6 +95,22 @@ module.exports = (area) => {
     })
 
     area.app.get('/oauth2/:service/check-token', async (req, res) => {
+        let token = req.headers['x-access-token'] || req.headers['authorization']
+
+        if (!token && req.query.token) {
+            token = "Bearer " + req.query.token
+        }
+
+        if (!token) {
+            return res.status(401).json({message: 'No token provided'})
+        }
+        const rawToken = token.split(' ')[1]
+        jwt.verify(rawToken, process.env.JWT_ACCESS_SECRET, {}, (err, decoded) => {
+            if (err || area.jwtDenyList.isTokenDenied(decoded.userId, decoded.iat)) {
+                return res.status(401).json({message: 'Unauthorized'})
+            }
+            req.jwt = decoded
+        })
 
         let userId = req.jwt.userId
 
