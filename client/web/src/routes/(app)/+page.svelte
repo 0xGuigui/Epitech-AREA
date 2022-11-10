d9a32b3a2e881b87de44efd6aca7d8b9f9668f8f
julien <julien.hennion@epitech.eu>
2022-11-10 23:56:27 +0100

    Fixed home page buttons

--- client/web/src/routes/(app)/+page.svelte
+++ client/web/src/routes/(app)/+page.svelte
@@ -23,11 +23,11 @@
         </div>
     {:then data}
         {#each data.server.services as {name, description}}
-            <div class="inline-grid w-[300px] h-[200px] my-7 ml-16 font-bold flex shadow-2xl justify-center items-center backdrop-blur-sm bg-white/60 rounded-2xl hover:scale-110 transition-all duration-150 select-none cursor-pointer">
+            <div on:click={() => {
+                 goto(`/services/${name}`);
+                 }}
+                 class="inline-grid w-[300px] h-[200px] my-7 ml-16 font-bold flex shadow-2xl justify-center items-center backdrop-blur-sm bg-white/60 rounded-2xl hover:scale-110 transition-all duration-150 select-none cursor-pointer">
                 <div class="w-full h-full justify-center items-center flex"
-                     on:click={() => {
-                       goto(`/services/${name}`);
-                     }}
                     >{name}
                 </div>
             </div>
