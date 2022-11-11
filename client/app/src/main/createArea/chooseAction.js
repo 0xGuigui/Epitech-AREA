02d09828243321c4ad7027f3ce654afdf0dcb8d6
Florian Garnier <florian.garnier@epitech.eu>
2022-11-11 10:14:02 +0100

    Changed emplacement of backhandlers

--- client/app/src/main/createArea/chooseAction.js
+++ client/app/src/main/createArea/chooseAction.js
@@ -34,7 +34,6 @@ export default function ChooseAction() {
 	}, [])
 
 	const backAction = () => {
-		navigate(history.prev)
 	}
 
 	useEffect(() => {
--- client/app/src/main/createArea/create.js
+++ client/app/src/main/createArea/create.js
@@ -1,15 +1,30 @@
-import {StyleSheet, Text, View, Linking} from "react-native";
+import {StyleSheet, Text, View, Linking, BackHandler} from "react-native";
 import {Pressable} from "@react-native-material/core";
 import {Appbar} from "react-native-paper";
 import {DarkTheme} from "../../../config";
 import * as React from "react";
 import * as WebBrowser from 'expo-web-browser'
-import {Route, Routes} from "react-router-native";
+import {Route, Routes, useNavigate} from "react-router-native";
 import DisplayMenu from "./displayMenu";
 import DisplayServices from "./displayServices";
 import ChooseAction from "./chooseAction";
+import {useContext, useEffect} from "react";
+import {HistoryContext} from "../../historyContext";
 
 export default function CreateArea() {
+	const navigate = useNavigate()
+	const history = useContext(HistoryContext)
+
+	const backAction = () => {
+		navigate(history.prev)
+	}
+
+	useEffect(() => {
+		BackHandler.addEventListener("hardwareBackPress", backAction);
+
+		return () =>
+			BackHandler.removeEventListener("hardwareBackPress", backAction);
+	}, [history])
 
 	return (
 		<View style={styles.mainSection}>
