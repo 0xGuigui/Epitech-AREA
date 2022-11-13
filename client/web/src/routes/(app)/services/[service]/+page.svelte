530e2e2096ee553dd4c05a8b91dfe735ebdfed62
Florian Garnier <florian.garnier@epitech.eu>
2022-11-13 22:39:45 +0100

    Added click on div to switch from name to desc

--- client/web/src/routes/(app)/services/[service]/+page.svelte
+++ client/web/src/routes/(app)/services/[service]/+page.svelte
@@ -10,11 +10,22 @@
     async function getServices() {
         const response = await fetch($serverUrl + '/about.json')
         const json = await response.json()
+	    console.log(json)
         return {
             status: response.status,
             ...json
         };
     }
+
+    let desc = []
+
+    function onClickHandler(action) {
+		if (desc.find(e => e === action.name))
+			desc = [...desc.filter(e => e !== action.name)]
+		else {
+			desc = [...desc, action.name]
+		}
+	}
 </script>
 
 <section>
@@ -36,29 +47,34 @@
                     </div>
                 </div>
                 <div class="flex flex-col justify-center items-center bg-white w-full">
-                    <p class="font-bold text-4xl text-area-header">Actions</p>
-                    <div class="w-[50px] h-[4px] bg-area-header text-black font-bold rounded-2xl"></div>
-                    <div class="w-full flex flex-wrap justify-center">
-                        {#each service.actions as action}
-                            <div style="background-color: {service.colorPalette.secondaryColor};"
-                                 class="inline-grid mx-5 flex my-5 w-[300px] h-[200px] font-bold text-white text-2xl shadow-2xl justify-center items-center rounded-2xl select-none cursor-pointer hover:scale-110 transition-all duration-300">
-                                {action}
-                            </div>
-                        {/each}
-                    </div>
-                    <p class="font-bold text-4xl text-area-header">Reactions</p>
-                    <div class="w-[50px] h-[4px] bg-area-header text-black font-bold rounded-2xl"></div>
-                    <div class="w-full flex flex-wrap justify-center">
-                        {#each service.reactions as reaction}
-                            <div style="background-color: {service.colorPalette.secondaryColor};"
-                                 class="mx-5 flex my-5 w-[300px] h-[200px] font-bold text-white text-2xl shadow-2xl justify-center items-center rounded-2xl select-none cursor-pointer hover:scale-110 transition-all duration-300">
-                                {reaction}
-                                <div    on:click={() => {service.name.actions.description}}
-                                        class="">
+                    {#if service.actions.length > 0}
+                        <p class="font-bold text-4xl text-area-header">Actions</p>
+                        <div class="w-[50px] h-[4px] bg-area-header text-black font-bold rounded-2xl"></div>
+                        <div class="w-full flex flex-wrap justify-center">
+                            {#each service.actions as action}
+                                <div style="background-color: {service.colorPalette.secondaryColor};"
+                                     class="inline-grid mx-5 flex my-5 w-[300px] h-[200px] font-bold text-white text-2xl shadow-2xl justify-center items-center rounded-2xl select-none cursor-pointer hover:scale-110 transition-all duration-300 text-center"
+                                     on:click={() => onClickHandler(action)}
+                                >
+                                    {desc.find(e => e === action.name) ? action.description: action.name}
                                 </div>
-                            </div>
-                        {/each}
-                    </div>
+                            {/each}
+                        </div>
+                    {/if}
+                    {#if service.reactions.length > 0}
+                        <p class="font-bold text-4xl text-area-header">Reactions</p>
+                        <div class="w-[50px] h-[4px] bg-area-header text-black font-bold rounded-2xl"></div>
+                        <div class="w-full flex flex-wrap justify-center">
+                            {#each service.reactions as reaction}
+                                <div style="background-color: {service.colorPalette.secondaryColor};"
+                                     class="mx-5 flex my-5 w-[300px] h-[200px] font-bold text-white text-2xl shadow-2xl justify-center items-center rounded-2xl select-none cursor-pointer hover:scale-110 transition-all duration-300 text-center"
+                                     on:click={() => onClickHandler(reaction)}
+                                >
+                                    {desc.find(e => e === reaction.name) ? reaction.description: reaction.name}
+                                </div>
+                            {/each}
+                        </div>
+                    {/if}
                 </div>
             {/if}
         {/each}
