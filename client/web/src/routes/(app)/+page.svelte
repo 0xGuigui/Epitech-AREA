4fc1e057f1c6363eb76e6ffee2eb15f3e5fbf2e9
julien <julien.hennion@epitech.eu>
2022-11-13 17:24:01 +0100

    Added Reactions and actions cards in service pages

--- client/web/src/routes/(app)/+page.svelte
+++ client/web/src/routes/(app)/+page.svelte
@@ -45,7 +45,7 @@
         {#each data.services as service}
             <div on:click={() => goto(`/services/${service.name}`)}
                  style="background-color: {service.colorPalette.secondaryColor};"
-                 class="inline-grid w-[300px] h-[200px] my-7 ml-16 font-bold text-white text-2xl flex shadow-2xl justify-center items-center backdrop-blur-sm bg-white/40 rounded-2xl hover:scale-110 transition-all duration-150 select-none cursor-pointer">
+                 class="inline-grid w-[300px] h-[200px] my-7 ml-16 font-bold text-white text-2xl flex shadow-2xl justify-center items-center backdrop-blur-sm bg-white/40 rounded-2xl hover:scale-110 transition-all duration-300 select-none cursor-pointer">
                 <img src={"/" + service.name + ".png"} onerror="this.onerror=null;this.src='/logo-area.png'" class="flex justify-center items-center h-20 w-20" alt="service"/>
                 <div class="w-full h-full justify-center items-center flex"
                     >{service.name}
