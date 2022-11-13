56590d9f35213401664644767415fe0a66c93a48
julien <julien.hennion@epitech.eu>
2022-11-13 01:40:14 +0100

    Added fetch user infos in profile page and fixed descs

---
+++ client/web/src/routes/(app)/me/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {areaFetch} from "../../../lib/utils/areaFetch";
+
+    export let data;
+
+    let userInfos = getUserInfos();
+    let user = data.user;
+
+    async function getUserInfos() {
+        const response = await areaFetch('/me')
+        const json = await response.json()
+        return {
+            status: response.status,
+            ...json
+        };
+    }
+    console.log(user);
+</script>
+
+<section class="h-[100vh] w-[100vw]">
+    {#await userInfos}
+        <p></p>
+    {:then userInfos}
+            <div class="flex justify-center items-center w-[300px] h-[200px] backdrop-blur-sm bg-white/40 rounded-2xl text-black font-bold">{userInfos.user.username}</div>
+    {/await}
+</section>
