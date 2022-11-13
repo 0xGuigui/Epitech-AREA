8d3c00f47d11ef3ebe8834fb70d22a2725dc24eb
Florian Garnier <florian.garnier@epitech.eu>
2022-11-13 21:40:19 +0100

    Added Discord login

--- client/web/src/lib/data/config.ts
+++ client/web/src/lib/data/config.ts
@@ -1,3 +1,38 @@
+const oauth2 = {
+    "Spotify": {
+        client_id: "e62780e3630e4439b855928a8514e977",
+        redirect_uri: "http://localhost:3000/oauth2/Spotify",
+        scopes: "user-read-private user-read-email user-read-playback-state user-modify-playback-state app-remote-control playlist-read-private",
+    },
+    "Discord": {
+        client_id: "1022518270202499072",
+        redirect_uri: "http://localhost:3000/oauth2/Discord",
+        scopes: "identify email guilds guilds.members.read connections",
+        login_scopes: "identify email"
+    },
+    "Reddit": {
+        client_id: "OYLm6Eh9OUu2VrJxtBs-mg",
+        redirect_uri: "http://localhost:3000/oauth2/Reddit",
+        scopes: "identity mysubreddits history privatemessages",
+        state: "0e7b5b87003476a68c081aaeb140b577127102a02ebe50925530be0cee86cb0043b371480397ab26050f7de5e0799488aa0d3d22d490dd4b70ac068a66757061d14fbe04392e92608b694c700443c4bf30f5b87a2807bfc816203426a24f06d1980180807db1aa0ce4b63890523a6c50b1270b480285538fe0a150ee19"
+    }
+}
+
 export default {
-    "defaultServerUrl": "http://92.148.23.72:8080"
+    "defaultServerUrl": "http://92.148.23.72:8080",
+    "Oauth2": {
+        "Spotify": {
+            ...oauth2.Spotify,
+            oauth_uri: `https://accounts.spotify.com/authorize?response_type=code&client_id=${oauth2.Spotify.client_id}&scope=${oauth2.Spotify.scopes}&redirect_uri=${oauth2.Spotify.redirect_uri}`
+        },
+        "Discord": {
+            ...oauth2.Discord,
+            oauth_uri: `https://discord.com/api/oauth2/authorize?client_id=${oauth2.Discord.client_id}&redirect_uri=${oauth2.Discord.redirect_uri}&response_type=code&scope=${oauth2.Discord.scopes}`,
+            login_uri: `https://discord.com/api/oauth2/authorize?client_id=${oauth2.Discord.client_id}&redirect_uri=${oauth2.Discord.redirect_uri}&response_type=code&scope=${oauth2.Discord.login_scopes}`
+        },
+        "Reddit": {
+            ...oauth2.Reddit,
+            oauth_uri: `https://www.reddit.com/api/v1/authorize.compact?client_id=${oauth2.Reddit.client_id}&response_type=code&state=${oauth2.Reddit.state}&redirect_uri=${oauth2.Reddit.redirect_uri}&duration=permanent&scope=${oauth2.Reddit.scopes}`
+        }
+    }
 }
--- client/web/src/routes/(oauth2)/oauth2/[service]/+page.svelte
+++ client/web/src/routes/(oauth2)/oauth2/[service]/+page.svelte
@@ -1,10 +1,51 @@
 <script lang="ts">
-    import {icons} from "../../../../lib/utils/fontAwesome";
+    import {icons} from "$lib/utils/fontAwesome";
     import Fa from "svelte-fa";
     import {page} from '$app/stores';
-    import {possibleIcons} from "../../../../lib/data/dynamicHeader";
+    import {possibleIcons} from "$lib/data/dynamicHeader";
+    import config from '$lib/data/config'
+    import {accessToken, loggedIn} from "../../../../store";
+    import {areaFetch} from "$lib/utils/areaFetch";
+    import {browser} from '$app/environment'
+
+    async function loginDiscord(code) {
+        const response = await fetch(`${config.defaultServerUrl}/login/Discord`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                code,
+                redirect_uri: config.Oauth2.Discord.redirect_uri
+            })
+        })
+        const json = await response.json()
+        console.log(json)
+        if (response.status === 200) {
+            if (browser) {
+                loggedIn.set(true)
+                accessToken.set(json.token)
+                localStorage.setItem("refreshToken", json.refreshToken);
+            }
+        }
+    }
+
+    async function stuff(code) {
+        const res = await areaFetch(`/oauth2/${service}`, 'POST', {"code": code, "redirect_uri": config.Oauth2[service].redirect_uri})
+        console.log(res)
+    }
 
     let service = $page.params.service;
+    const code = $page.url.searchParams.get('code')
+
+    if (browser) {
+        console.log(localStorage.getItem('discord_login'))
+        if (localStorage.getItem('discord_login') === "true") {
+            loginDiscord(code)
+            localStorage.removeItem('discord_login')
+        } else
+            stuff(code)
+    }
 
     service = service.charAt(0).toUpperCase() + service.slice(1);
 </script>
@@ -15,6 +56,6 @@
         <span class="text-white text-3xl ml-3 font-bold">AREA</span>
     </a>
     <Fa icon={possibleIcons[service] || icons.faMarker} size="10x" color="#fff" />
-    <div class="mt-8 text-5xl text-white font-bold">connected to <span class="area-letter">{$page.params.service}</span></div>
+    <div class="mt-8 text-5xl text-white font-bold">Connected to <span class="area-letter">{$page.params.service}</span></div>
     <div class="text-white text-xl mt-2">(you can close this window)</div>
 </section>
