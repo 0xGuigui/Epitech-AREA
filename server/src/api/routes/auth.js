a345b3b635d2167b50ad16d44ab44a395cb89ecc
Florian Garnier <florian.garnier@epitech.eu>
2022-11-13 02:16:39 +0100

    Added login route for discord oauth

--- server/src/api/routes/auth.js
+++ server/src/api/routes/auth.js
@@ -6,10 +6,37 @@ const {
     loginSchema,
     registerSchema,
     sendResetPasswordEmailSchema,
-    resetPasswordSchema
+    resetPasswordSchema, registerServiceSchema, loginDiscordSchema
 } = require('../models/joi/authSchemas')
 const {validatePayload} = require('../middlewares/dynamic')
 const mongoose = require('mongoose')
+
+async function getRefreshToken(code, redirect_uri) {
+    const response = await fetch(`https://discord.com/api/oauth2/token`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            client_id: process.env.DISCORD_CLIENT_ID,
+            client_secret: process.env.DISCORD_CLIENT_SECRET,
+            redirect_uri,
+            code,
+            grant_type: 'authorization_code'
+        })
+    })
+    return await response.json()
+}
+
+async function getMe(access_token) {
+    const response = await fetch(`https://discord.com/api/v10/users/@me`, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${access_token}`
+        }
+    })
+    return await response.json()
+}
 
 module.exports = (area) => {
     area.app.post('/register', validatePayload(registerSchema), async (req, res) => {
@@ -79,6 +106,59 @@ module.exports = (area) => {
                 refreshToken: refreshToken,
             })
         })
+    })
+
+    area.app.post('/login/Discord', validatePayload(loginDiscordSchema), async (req, res) => {
+        const token = await getRefreshToken(req.body.code, req.body.redirect_uri)
+
+        if (!token)
+            return res.status(401).json({message: 'Invalid verification code'})
+
+        const user = await getMe(token.access_token)
+
+        const sendToken = (payload, accessToken) => jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '3h'}, (err, refreshToken) => {
+            if (err) {
+                return res.status(500).send({error: 'Internal server error'})
+            }
+            res.cookie('jwt', refreshToken, {
+                maxAge: 3 * 60 * 60 * 1000
+            });
+            res.status(200).send({
+                token: accessToken,
+                refreshToken: refreshToken,
+            })
+        })
+
+        const dbUser = await mongoose.models.User.findOne({oauth: user.id}).exec()
+
+        if (!dbUser) {
+            new mongoose.models.User({
+                username: user.username,
+                email: user.email,
+                oauth: user.id
+            }).save().then(newUser => {
+                const payload = {
+                    userId: newUser.id,
+                    username: user.username,
+                    admin: newUser.admin
+                }
+                const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '5m'}, null)
+                sendToken(payload, accessToken)
+            }).catch(err => {
+                if (err.code === 11000) {
+                    return res.status(409).json({message: 'User already exists'})
+                }
+                return res.status(500).send(err)
+            })
+        } else {
+            const payload = {
+                userId: dbUser.id,
+                username: user.username,
+                admin: dbUser.admin
+            }
+            const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '5m'}, null)
+            sendToken(payload, accessToken)
+        }
     })
 
     area.app.post('/refresh', (req, res) => {
